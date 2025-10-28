9d00dc297bb5c96dfa26c3072d0e453797c8c9cd
Taylor-demo <3427623891@qq.com>
2025-10-28 17:23:31 +0800

    10_28study

---
+++ C++ Primer Plus/第二章 开始学习C++/Project1/10_28.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "10_28.h"
+
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it !" << endl;
+	return 0;
+}
---
+++ C++ Primer Plus/第二章 开始学习C++/Project1/10_28.h
@@ -0,0 +1,5 @@
+#ifndef __10_28.H__
+#define __10_28.H__
+
+
+#endif
